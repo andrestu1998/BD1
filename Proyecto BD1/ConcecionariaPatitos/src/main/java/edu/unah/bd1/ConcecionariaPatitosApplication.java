98abc3cac5d3082c01bb22457582a5b009df6c3b
Andrés Ulate <60900373+andrestu1998@users.noreply.github.com>
2021-07-06 14:36:59 -0600

    Add files via upload

---
+++ Proyecto BD1/ConcecionariaPatitos/src/main/java/edu/unah/bd1/ConcecionariaPatitosApplication.java
@@ -0,0 +1,13 @@
+package edu.unah.bd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcecionariaPatitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcecionariaPatitosApplication.class, args);
+	}
+
+}
---
+++ Proyecto BD1/ConcecionariaPatitos/src/test/java/edu/unah/bd1/ConcecionariaPatitosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unah.bd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcecionariaPatitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
